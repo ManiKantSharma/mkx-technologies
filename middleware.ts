8ce545eb2481df00d7b024545f37ba9f19fc2a07
Mani Kant Sharma <manikants157@gmail.com>
2026-05-16 20:06:10 +0530

    Initial Commit

--- middleware.ts
+++ middleware.ts
@@ -1,46 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
-import { jwtVerify } from "jose";
+
 
 export default async function middleware(request: NextRequest) {
-  const JWT_SECRET = process.env.JWT_SECRET;
-  if (!JWT_SECRET) {
-    return NextResponse.next();
-  }
-  const secret = new TextEncoder().encode(JWT_SECRET);
-  const protectedPaths = ["/admin"];
-  const publicPaths = ["/", "/login", "/signup", "/forgot-password"];
-
-  const { pathname } = request.nextUrl;
-
-  const isProtectedPath = protectedPaths.some((path) =>
-    pathname.startsWith(path),
-  );
-  const isPublicPath = publicPaths.some((path) => pathname === path);
-
-  if (isPublicPath) {
-    return NextResponse.next();
-  }
-
-  if (isProtectedPath) {
-    const token = request.cookies.get("auth-token")?.value;
-
-    if (!token) {
-      return NextResponse.redirect(new URL("/login", request.url));
-    }
-
-    try {
-      const { payload } = await jwtVerify(token, secret);
-
-      if (pathname.startsWith("/admin") && payload.role !== "admin") {
-        return NextResponse.redirect(new URL("/login", request.url));
-      }
-
-      return NextResponse.next();
-    } catch (error) {
-      return NextResponse.redirect(new URL("/login", request.url));
-    }
-  }
-
   return NextResponse.next();
 }
 
